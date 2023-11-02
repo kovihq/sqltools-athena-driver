de8c869c92da4127062d8387c081834179b11aae
Johni Michels <johni.michels@gmail.com>
2023-11-01 23:35:02 -0300

    fixed driver ref

--- src/ls/plugin.ts
+++ src/ls/plugin.ts
@@ -1,11 +1,11 @@
 import { ILanguageServerPlugin } from '@sqltools/types';
-import YourDriver from './driver';
+import AthenaDriver from './driver';
 import { DRIVER_ALIASES } from './../constants';
 
 const YourDriverPlugin: ILanguageServerPlugin = {
   register(server) {
     DRIVER_ALIASES.forEach(({ value }) => {
-      server.getContext().drivers.set(value, YourDriver as any);
+      server.getContext().drivers.set(value, AthenaDriver as any);
     });
   }
 }
